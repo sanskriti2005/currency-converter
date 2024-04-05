63879e5073752452f411079495eef54397ae721e
Sanskriti <sanskritisri27@gmail.com>
2024-04-05 03:33:13 +0000

    added code to take arguments

--- converter.py
+++ converter.py
@@ -1,3 +1,4 @@
+import argparse
 from configparser import ConfigParser
 
 # FETCHES API KEY
@@ -6,3 +7,26 @@ def _get_api_key():
     config.read("secrets.ini")
     return config["freecurrencyapi"]["api_key"]
 
+#
+def read_user_cli_args():
+    parser = argparse.ArgumentParser(
+        description = "gets currency and the target currency for conversion"
+    )
+    parser.add_argument(
+        "currency", type = str, help="enter a currency."
+    )
+    parser.add_argument(
+        "target_currency", type=str, help="enter a target currency"
+    )
+    parser.add_argument(
+        "-c",
+        "--currency_rate",
+        action="store_true",
+        help="display the currency rate"
+
+    )
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    read_user_cli_args()
